30e24f3a6dbea12a380f304582943b9a904cdd1d
You <you@example.com>
2024-10-24 11:16:42 +0800

    Add feature: cal_TKE(time, domain_range). This function calculates the turbulent kinetic energy (TKE) over a given area at a given time. For the additional arguments: Conversion to a-grid can be turn off, while printing shape info can be turn on for checking purposes.

--- vvmtoolsV2.py
+++ vvmtoolsV2.py
@@ -4,6 +4,7 @@ import pandas as pd
 import xarray as xr
 from datetime import *
 import glob
+import logging
 import vvmtools as vvmtools_aaron  # vvmtools V1
 
 # --- Build Class --- #
@@ -59,17 +60,58 @@ class VVMtools(vvmtools_aaron.VVMTools):
             th_org      = self.get_var('th', time, numpy=True)
             return th_org[1:, :-1, :-1]       # discard the first z, last y, last x
         
+    def cal_TKE(self, time, domain_range, 
+                conv_agrid:bool=True, 
+                print_shape:bool=False):
+        """
+        Calculate TKE over the designated domain range and on the specified time step.
+        Default:  u, v, w will be converted to a-grid before calculation.
+        Default:  Calculate domain-average. TKE is only representative of turbulent motions when considering an area.
+        Optional: Set print_shape=True to get the shape info of the retrieved u, v, w.
+        """
+        k1, k2, j1, j2, i1, i2 = domain_range
+        if conv_agrid:
+            # Check domain_range on the x-direction
+            if i2 is not None and (i2 > 63):
+                logging.warning(f"Grid {i2} on the x-axis is on/over the borderline "+ 
+                                f"between two landtypes after interpolation (a-grid), "+ 
+                                f"automatically set to 63.")
+                i2           = 63
+                domain_range = (k1, k2, j1, j2, i1, i2)
+            if i1 is not None and (i1 < 64):
+                logging.warning(f"Grid {i1} on the x-axis is on/over the borderline "+ 
+                                f"between two landtypes after interpolation (a-grid), "+ 
+                                f"automatically set to 64.")
+                i1           = 64
+                domain_range = (k1, k2, j1, j2, i1, i2)
+            # Get converted u, v, w
+            u = self.convert_to_agrid('u', time)[k1:k2, j1:j2, i1:i2].copy()
+            v = self.convert_to_agrid('v', time)[k1:k2, j1:j2, i1:i2].copy()
+            w = self.convert_to_agrid('w', time)[k1:k2, j1:j2, i1:i2].copy()
+        else:      
+            u = np.squeeze(self.get_var("u", time, domain_range, numpy=True))
+            v = np.squeeze(self.get_var("v", time, domain_range, numpy=True))
+            w = np.squeeze(self.get_var("w", time, domain_range, numpy=True))
+        # Provide shape info
+        if print_shape: print("Shape of u, v, w:", u.shape, v.shape, w.shape)
+
+        # POSSIBLE TODO: argument xarray:bool 
+        # -> might be more convenient to entail variable info
+        
+        return np.nanmean((u**2+v**2+w**2)/2, axis=(1, 2))
+        
 # --- Test --- #
 if __name__ == "__main__":
     test_case     = '/data/mlcloud/ch995334/VVM/DATA/pbl_mod_wfire_coastal_s1/'
     test_instance = VVMtools(test_case)
     # Annoucing function testing
-    print("Function testing: convert_to_agrid")
+    print("Function testing: cal_TKE")
     # Necessary variables and get result
-    test_var    = 'eta'
+    # test_var    = 'eta'
     time_step   = 100
-    test_result = test_instance.convert_to_agrid(var=test_var, time=time_step)
+    test_range  = (None, None, None, None, 64, None)
+    test_result = test_instance.cal_TKE(time=time_step, domain_range=test_range, print_shape=True, conv_agrid=False)
     # Testing result
-    print("Var:", test_var, "time_step:", time_step)
+    print("time_step:", time_step, "domain_range:", test_range)
     print(test_result)
     print(test_result.shape)
