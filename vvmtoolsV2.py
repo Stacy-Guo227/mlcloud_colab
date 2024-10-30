e620fc83935cd5e5994cdd2a925d2322a578b1af
You <you@example.com>
2024-10-30 22:14:42 +0800

    Add description: all current methods.

--- vvmtoolsV2.py
+++ vvmtoolsV2.py
@@ -17,7 +17,19 @@ class VVMtools(vvmtools_aaron.VVMTools):
     
     def convert_to_agrid(self, var, time, domain_range=(None, None, None, None, None, None)):
         """
+        Convert the six dynamic variables (u, v, w, eta, xi, zeta) to a-grid. 
+        After interpolation, the corresponding dimension would reduce by 1 grid at a time, the first grid on the uninterpolated dimensions would be omitted to acquire equal dimension outputs.  
+        Theta is an acceptable input for dimension adjustment.
         
+        :param var: Variable name
+        :type  var: str
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        
+        :return: The interpolated data.
+        :rtype : np.ndarray
         """
         # Wind field (u, v, w)
         if var == 'u':
@@ -61,13 +73,20 @@ class VVMtools(vvmtools_aaron.VVMTools):
             return th_org[1:, 1:, 1:]         # discard the first z, y, x
         
     def cal_TKE(self, time, domain_range, 
-                conv_agrid:bool=True, 
-                print_shape:bool=False):
+                conv_agrid:bool=True):
         """
-        Calculate TKE over the designated domain range and on the specified time step.
-        Default:  u, v, w will be converted to a-grid before calculation.
-        Default:  Calculate domain-average. TKE is only representative of turbulent motions when considering an area.
-        Optional: Set print_shape=True to get the shape info of the retrieved u, v, w.
+        Calculate area-averaged Turbulent Kinetic Energy for designated domain range and time step.
+        (TKE is only representative of turbulent motions when considering an area.)
+        
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        :param conv_agrid: Perform this calculation on a-grid.
+        :type  conv_agrid: bool, optional, default=True
+        
+        :return: TKE
+        :rtype : np.ndarray
         """
         if conv_agrid:
             u = self.convert_to_agrid('u', time, domain_range)
@@ -77,8 +96,6 @@ class VVMtools(vvmtools_aaron.VVMTools):
             u = np.squeeze(self.get_var("u", time, domain_range, numpy=True))
             v = np.squeeze(self.get_var("v", time, domain_range, numpy=True))
             w = np.squeeze(self.get_var("w", time, domain_range, numpy=True))
-        # Provide shape info
-        if print_shape: print("Shape of u, v, w:", u.shape, v.shape, w.shape)
 
         # POSSIBLE TODO: argument xarray:bool 
         # -> might be more convenient to entail variable info
@@ -86,13 +103,19 @@ class VVMtools(vvmtools_aaron.VVMTools):
         return np.nanmean((u**2+v**2+w**2)/2, axis=(1, 2))
     
     def cal_enstrophy(self, time, domain_range, 
-                      conv_agrid:bool=True, 
-                      print_shape:bool=False):
+                      conv_agrid:bool=True):
         """
-        Calculate enstrophy over the designated domain range and on the specified time step.
-        Default:  eta, xi, zeta will be converted to a-grid before calculation.
-        Default:  Calculate domain-average. Enstrophy is only representative of turbulent motions when considering an area.
-        Optional: Set print_shape=True to get the shape info of the retrieved eta, xi, zeta.
+        Calculate enstrophy for designated domain range and specified time step.
+        
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        :param conv_agrid: Perform this calculation on a-grid.
+        :type  conv_agrid: bool, optional, default=True
+        
+        :return: enstrophy
+        :rtype : np.ndarray
         """
         if conv_agrid:
             eta  = self.convert_to_agrid('eta', time, domain_range)
@@ -108,8 +131,6 @@ class VVMtools(vvmtools_aaron.VVMTools):
             ## Other components of vorticity
             xi   = np.squeeze(self.get_var("xi", time, domain_range, numpy=True))
             zeta = np.squeeze(self.get_var("zeta", time, domain_range, numpy=True))
-        # Provide shape info
-        if print_shape: print("Shape of eta, xi, zeta:", eta.shape, xi.shape, zeta.shape)
 
         # POSSIBLE TODO: argument xarray:bool 
         # -> might be more convenient to entail variable info
@@ -120,9 +141,21 @@ class VVMtools(vvmtools_aaron.VVMTools):
                       wind_var, prop_var,
                       conv_agrid:bool=True):
         """
-        Params:
-        wind_var is the medium [u/v/w].
-        prop_var is the property transported by the medium.
+        Calculate turbulent flux for designated domain_range and time step based on (a-a_bar)*(b-b_bar).
+        
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        :param wind_var: Medium variable (mostly u/v/w)
+        :type  wind_var: str
+        :param prop_var: Property variable, being transported by the medium
+        :type  prop_var: str
+        :param conv_agrid: Perform this calculation on a-grid.
+        :type  conv_agrid: bool, optional, default=True
+        
+        :return: turbulent flux
+        :rtype : np.ndarray
         """
         if conv_agrid:
             windvar  = self.convert_to_agrid(wind_var, time)
@@ -145,6 +178,17 @@ class VVMtools(vvmtools_aaron.VVMTools):
         return (windreg-wind_bar)*(propreg-prop_bar)
         
     def _find_levels_1d(self, var, conv_agrid:bool):
+        """
+        Find the level where var=var[z=0]+0.5. Used by finding PBL height with theta+0.5k.
+        
+        :param var: Variable name (normally theta, 'th')
+        :type  var: str
+        :param conv_agrid: Perform this calculation on a-grid, the rendered height would be based on zc with first layer omitted. 
+        :type  conv_agrid: bool
+        
+        :return: height
+        :rtype : float
+        """
         target_var = var[0] + 0.5
         zidx       = np.argmax(var >= target_var)
         zidx       = np.where(np.any(var >= target_var), zidx, 0)   # set to 0 if no level satifies the condition
@@ -157,9 +201,19 @@ class VVMtools(vvmtools_aaron.VVMTools):
                           compute_mean_axis=None,
                           conv_agrid:bool=True):
         """
-        Default: No average. If compute_mean_axis is activated, theta mean is calculated before rendering PBL height.
-        params:
-        compute_mean_axis: can be None (default), 'x', 'y', 'xy'
+        Find the PBL height based on levels meeting first level theta+0.5K.
+        
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        :param compute_mean_axis: If assigned, mean-theta would be calculated before rendering PBL height. Options include ['x', 'y', 'xy']
+        :type  compute_mean_axis: str, optional, default=None
+        :param conv_agrid: Perform this calculation on a-grid.
+        :type  conv_agrid: bool, optional, default=True
+        
+        :return: PBL height(s)
+        :rtype : float or np.ndarray
         """
         if conv_agrid:
             z            = self.DIM['zc'][1:]
@@ -187,6 +241,21 @@ class VVMtools(vvmtools_aaron.VVMTools):
     def _pbl_height_dthtz(self, time, domain_range,
                           compute_mean_axis=None,
                           conv_agrid:bool=True):
+        """
+        Find the PBL height based on levels with maximum theta gradient.
+        
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        :param compute_mean_axis: If assigned, mean-theta would be calculated before rendering PBL height. Options include ['x', 'y', 'xy']
+        :type  compute_mean_axis: str, optional, default=None
+        :param conv_agrid: Perform this calculation on a-grid.
+        :type  conv_agrid: bool, optional, default=True
+        
+        :return: PBL height(s)
+        :rtype : float or np.ndarray
+        """
         if conv_agrid:
             z            = self.DIM['zc'][1:]
             th           = self.convert_to_agrid('th', time, domain_range)
@@ -218,7 +287,20 @@ class VVMtools(vvmtools_aaron.VVMTools):
                         threshold:float, 
                         conv_agrid:bool=True):
         """
-        Default: domain-average (from the calculation of flux)
+        Find the PBL height based on levels with vertical theta flux changeing signs and its minimum.
+        Only area-averaged result is available, by definition.
+        
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        :param threshold: Magnitude to be considered valid
+        :type  threshold: float
+        :param conv_agrid: Perform this calculation on a-grid.
+        :type  conv_agrid: bool, optional, default=True
+        
+        :return: PBL height
+        :rtype : float
         """
         if conv_agrid:
             z            = self.DIM['zc'][1:]
@@ -231,7 +313,24 @@ class VVMtools(vvmtools_aaron.VVMTools):
                        method:str, threshold:float=0., compute_mean_axis=None, 
                        conv_agrid:bool=True):
         """
-        Available method now includes: 'wth', 'th05k', 'dthdz'
+        Find the PBL height based on the designated definition.
+        Currently available: th05k, max. theta gradient, vertical theta flux profile, certain value of TKE, and certain value of enstrophy.
+        
+        :param time: Designated time step
+        :type  time: int
+        :param domain_range: Designated spatial range, dimension follows (z, y, x)
+        :type  domain_range: tuple, optional, default:(None, None, None, None, None, None)
+        :param method: PBL height definition options
+        :type  method: str
+        :param threshold: Magnitude to be considered valid (meaningless for th05k and dthdz)
+        :type  threshold: float, optional, default=0.
+        :param compute_mean_axis: If assigned, mean-theta would be calculated before rendering PBL height. Options include ['x', 'y', 'xy'] (meaningless for wth, tke, enstrophy)
+        :type  compute_mean_axis: str, optional, default=None
+        :param conv_agrid: Perform this calculation on a-grid.
+        :type  conv_agrid: bool, optional, default=True
+        
+        :return: PBL height(s)
+        :rtype : float or np.ndarray
         """
         if method == 'wth':
             heights = self._pbl_height_wth(time, domain_range, conv_agrid)
